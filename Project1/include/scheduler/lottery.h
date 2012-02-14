2a1701c354d4cbc22f49f2fa6d2600165f170a77
Dylan Bettermann <bett0159@umn.edu>
2012-02-14 14:18:44 -0600

    Copied FCFS code into Lottery scheduler. Basically placeholder code for now.

--- Project1/include/scheduler/lottery.h
+++ Project1/include/scheduler/lottery.h
@@ -5,10 +5,49 @@
  *      Author: Dylan
  */
 
-#ifndef LOTTERY_H_
-#define LOTTERY_H_
+#ifndef LOTTERY_H_INCLUDED
+#define LOTTERY_H_INCLUDED
 
+#include <cstdlib>
+#include <queue>
+#include <assert.h>
+#include <pthread.h>
 
+#include "scheduler/scheduler.h"
+#include "utility/id.h"
 
+using namespace std;
+
+class cLottery: public cScheduler {
+private:
+	queue<ProcessInfo*> readyQueue;
+	vector<ProcessInfo*> blockedVector;
+
+	ProcessInfo* runningProc;
+
+	pthread_mutex_t blockedLock;
+	pthread_cond_t allBlocked;
+
+	cIDManager blockedID;
+
+public:
+	cLottery();
+	~cLottery();
+
+	void initProcSceduleInfo(ProcessInfo*);
+	void addProcess(ProcessInfo*);
+	void setBlocked(ProcessInfo*);
+	void unblockProcess(ProcessInfo*);
+	void removeProcess(ProcessInfo*);
+
+	ProcessInfo* getNextToRun();
+
+	pidType numProcesses();
+
+};
+
+struct lotteryInfo {
+	unsigned int blockedIndex;
+};
 
 #endif /* LOTTERY_H_ */
