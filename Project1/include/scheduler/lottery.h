1bfedc9a052af913e44506fd194ef6bc533fcc10
Dylan Bettermann <bett0159@umn.edu>
2012-02-14 13:56:47 -0600

    Added lottery.h and lottery.cpp. May already exist, but I did not see them.

---
+++ Project1/include/scheduler/lottery.h
@@ -0,0 +1,14 @@
+/*
+ * lottery.h
+ *
+ *  Created on: Feb 14, 2012
+ *      Author: Dylan
+ */
+
+#ifndef LOTTERY_H_
+#define LOTTERY_H_
+
+
+
+
+#endif /* LOTTERY_H_ */
---
+++ Project1/src/scheduler/lottery.cpp
@@ -0,0 +1,10 @@
+/*
+ * lottery.cpp
+ *
+ *  Created on: Feb 14, 2012
+ *      Author: Dylan
+ */
+
+
+
+
