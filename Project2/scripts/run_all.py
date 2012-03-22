c1257b4b5b8b961a9107e5722539641ad68e77bb
Planeman <planemantheace@gmail.com>
2012-03-22 11:36:19 -0500

    Updated run_all script to also execute the graph generator.

--- Project2/scripts/run_all.py
+++ Project2/scripts/run_all.py
@@ -3,7 +3,7 @@
 import os,re,sys
 
 if ( len(sys.argv) < 3 ):
-	print("Usage: %s <bindir> <test_configs_dirname>" % (sys.argv[0]))
+	print("Usage: %s <bindir> <test_configs_dirname> <optional: output_dir>" % (sys.argv[0]))
 	exit(-1)
 
 if (not os.chdir(sys.argv[1])):
@@ -17,9 +17,17 @@ if (test_dirs == None):
 	print("Failed to get listing for test_configs folder")
 	exit(-1)
 
+if ( len(sys.argv) > 3 ):
+	output_dirs = os.listdir(sys.argv[3])
+else:
+	output_dirs = None
+
 make_re = re.compile("^(?:M|m)ake.*$")
 
 full_configdir_path = os.path.join(cwd, sys.argv[2])
+
+if ( output_dirs != None ):
+	full_outdir_path = os.path.join(cwd, sys.argv[3])
 
 for dir in test_dirs:
 	if not os.path.isdir(os.path.join(full_configdir_path, dir)):
@@ -47,3 +55,20 @@ for dir in test_dirs:
 	##Otherwise execute the makefile
 	print("Executing: " + "make -f " + os.path.join(test_path,makefile))
 	os.system("make -f %s" % (os.path.join(test_path, makefile)))
+
+if output_dirs == None:
+	exit(0)
+
+for dir in output_dirs:
+	if not os.path.isdir(os.path.join(full_outdir_path,dir)):
+		print("Skipping (%s): not a directory" % (dir))
+		continue
+		
+	out_path = os.path.join(sys.argv[3], dir)
+	out_contents = os.listdir(out_path)
+	
+	graphMaker = "../scripts/makeGraphs.py"
+	print("Executing: " + "python %s %s" % (graphMaker, out_path))
+	os.system("python %s %s" % (graphMaker,out_path))
+	
+	
